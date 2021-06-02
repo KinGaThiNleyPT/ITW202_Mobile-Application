14271e43695cb0e7c2cbb9e651fc192a313f815d
KinGaThiNleyPT <12190071.gcit@rub.edu.bt>
2021-06-02 10:06:48 +0600

    SQLITE

--- TODO_25/app/src/main/java/gcit/edu/bt/todo_25/DatabaseHelper.java
+++ TODO_25/app/src/main/java/gcit/edu/bt/todo_25/DatabaseHelper.java
@@ -2,49 +2,76 @@ package gcit.edu.bt.todo_25;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME ="student.db";
-    public static final String TABLE_NAME ="Student_table";
-    public static final String COL_1 ="ID";
-    public static final String COL_2 ="NAME";
-    public static final String COL_3 ="SURNAME";
-    public static final String COL_4 ="MARKS";
+    public static final String DATABASE_NAME = "Student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "FIRSTNAME";
+    public static final String COL_3 = "LASTNAME";
+    public static final String COL_4 = "MARKS";
 
-
-    public DatabaseHelper(Context context){
-        super(context,DATABASE_NAME,null,1);
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(" create table "+ TABLE_NAME + "(ID INTEGER PRIMARY KEY,"+ "NAME TEXT, SURNAME TEXT, MARKS INTEGER)");
-
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY," +
+                "FIRSTNAME TEXT, LASTNAME TEXT, MARKS INTEGER)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " +TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
 
-    public boolean insertData(String id, String fname, String lname, String marks) {
+    public boolean insertData(String id, String firstname, String lastname, String marks) {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues =new ContentValues();
-        contentValues.put(COL_1,id);
-        contentValues.put(COL_2,fname);
-        contentValues.put(COL_3,lname);
-        contentValues.put(COL_4,marks);
-
-        long result =db.insert(TABLE_NAME,null,contentValues);
-        if (result==-1){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, firstname);
+        contentValues.put(COL_3, lastname);
+        contentValues.put(COL_4, marks);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1){
             return false;
         }
         else {
             return true;
         }
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+
+        return res;
+    }
+
+    public boolean updateData(String id, String fname, String lname, String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, fname);
+        contentValues.put(COL_3, lname);
+        contentValues.put(COL_4, marks);
+        long result = db.update(TABLE_NAME, contentValues, "ID = ?", new String[] {id});
+
+        if (result != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Integer deleteData (String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[] {id});
     }
 }
--- TODO_25/app/src/main/java/gcit/edu/bt/todo_25/MainActivity.java
+++ TODO_25/app/src/main/java/gcit/edu/bt/todo_25/MainActivity.java
@@ -1,41 +1,96 @@
 package gcit.edu.bt.todo_25;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseHelper mydb;
-    public EditText FirstName, LastName, Marks, Enroll;
+    DatabaseHelper myDb;
+    private EditText Fname, Lname, Marks, id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirstName=findViewById(R.id.firstname);
-        LastName=findViewById(R.id.lastname);
-        Marks =findViewById(R.id.marks);
-        Enroll =findViewById(R.id.StudentID);
+        myDb = new DatabaseHelper(this);
+
+        Fname = findViewById(R.id.firstName);
+        Lname = findViewById(R.id.lastName);
+        Marks = findViewById(R.id.marks);
+        id = findViewById(R.id.idNumber);
 
     }
 
     public void AddData(View view) {
-        boolean isInserted = mydb.insertData(Enroll.getText().toString(),
-        FirstName.getText().toString(),
-        LastName.getText().toString(),
-        Marks.getText().toString());
+        boolean isInserted = myDb.insertData(id.getText().toString(),
+                Fname.getText().toString(),
+                Lname.getText().toString(),
+                Marks.getText().toString());
 
-        if (isInserted ==true){
-            Toast.makeText(getApplicationContext(),"Data inserted",Toast.LENGTH_SHORT).show();
+        if(isInserted == true) {
+            Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
         }
         else {
-            Toast.makeText(getApplicationContext(),"Data not inserted",Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Data Not Inserted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void ViewData(View view) {
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0) {
+            //show message
+            showMessage("Error", "Nothing found");
+            return;
         }
 
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            buffer.append("Student Id : "+res.getString(0)+"\n");
+            buffer.append("First Name : "+res.getString(1)+"\n");
+            buffer.append("Last Name : "+res.getString(2)+"\n");
+            buffer.append("ITW202 Marks : "+res.getString(3)+"\n\n");
+        }
 
+        //show all data
+        showMessage("List of Students", buffer.toString());
+    }
+
+    public void showMessage(String title, String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+
+
+    public void UpdateData(View view) {
+        boolean isUpdate = myDb.updateData(id.getText().toString(),
+                Fname.getText().toString(),
+                Lname.getText().toString(),
+                Marks.getText().toString());
+
+        if (isUpdate == true){
+            Toast.makeText(this, "Data Updated", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Data Not Updated", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void DeleteData(View view) {
+        Integer deletedRows = myDb.deleteData(id.getText().toString());
+
+        if (deletedRows > 0)
+            Toast.makeText(this, "Data Deleted", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Data Not Deleted", Toast.LENGTH_SHORT).show();
     }
 }
